1708cd90d32213818d9109928cf8991220eb98c3
impichler <impichler@thewildcats.net>
2026-05-06 13:24:28 -0400

    CHILDREN.

--- src/main/java/com/phantomstormx/_036java/GameController.java
+++ src/main/java/com/phantomstormx/_036java/GameController.java
@@ -6,8 +6,36 @@ import javafx.scene.layout.StackPane;
 
 public class GameController {
     @FXML
-    private Label welcomeText;
+    private StackPane tile1;
     @FXML
-    private StackPane tileOne;
+    private StackPane tile2;
+    @FXML
+    private StackPane tile3;
+    @FXML
+    private StackPane tile4;
+    @FXML
+    private StackPane tile5;
+    @FXML
+    private StackPane tile6;
+    @FXML
+    private StackPane tile7;
+    @FXML
+    private StackPane tile8;
+    @FXML
+    private StackPane tile9;
+    @FXML
+    private StackPane tile10;
+    @FXML
+    private StackPane tile11;
+    @FXML
+    private StackPane tile12;
+    @FXML
+    private StackPane tile13;
+    @FXML
+    private StackPane tile14;
+    @FXML
+    private StackPane tile15;
+    @FXML
+    private StackPane tile16;
 
 }

ce6fd7597e05d28ece329e18c67debc540938737
eemorris <eemorris@thewildcats.net>
2026-05-14 08:34:54 -0400

    play again button

--- src/main/java/com/phantomstormx/_036java/GameController.java
+++ src/main/java/com/phantomstormx/_036java/GameController.java
@@ -1,6 +1,7 @@
 package com.phantomstormx._036java;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+//import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
@@ -11,13 +12,17 @@ import javafx.util.Duration;
 public class GameController {
 
     // initializes objects from scene builder to controller
-    @FXML private StackPane tile1,  tile2,  tile3,  tile4;
-    @FXML private StackPane tile5,  tile6,  tile7,  tile8;
-    @FXML private StackPane tile9,  tile10, tile11, tile12;
-    @FXML private StackPane tile13, tile14, tile15, tile16;
-    @FXML private Button playAgain;
-    @FXML private TextField score;
+    @FXML
+    private StackPane tile1, tile2, tile3, tile4;
+    @FXML
+    private StackPane tile5, tile6, tile7, tile8;
+    @FXML
+    private StackPane tile9, tile10, tile11, tile12;
+    @FXML
+    private StackPane tile13, tile14, tile15, tile16;
+    //@FXML private Button playAgain;
     private GameEngine engine;
+    private GameController controller;
     @FXML
     private StackPane[][] tiles;
 
@@ -25,20 +30,19 @@ public class GameController {
     public void initialize() {
         engine = new GameEngine(); // starts the engine (spawns 2 tiles)
 
-        tiles = new StackPane[][] { // maps the 16 child StackPane(aka tiles) into a 2D array
-                { tile1,  tile2,  tile3,  tile4  },
-                { tile5,  tile6,  tile7,  tile8  },
-                { tile9,  tile10, tile11, tile12 },
-                { tile13, tile14, tile15, tile16 }
+        tiles = new StackPane[][]{ // maps the 16 child StackPane(aka tiles) into a 2D array
+                {tile1, tile2, tile3, tile4},
+                {tile5, tile6, tile7, tile8},
+                {tile9, tile10, tile11, tile12},
+                {tile13, tile14, tile15, tile16}
         };
 
         /*
         This ensures that the key listener is added only after tile1 spawns which handles keyboard events in JavaFX,
         stops the event from reaching any further nodes or handlers
          */
-
         tile1.sceneProperty().addListener((obs, oldScene, newScene) -> {
-            if (newScene != null) { // :: acts as a listener
+            if (newScene != null) {
                 newScene.addEventFilter(KeyEvent.KEY_PRESSED, this::handleKeyPress); // intercepts events during the early "capture" phase of the event dispatch chain
             }
         });
@@ -49,31 +53,26 @@ public class GameController {
 
         switch (e.getCode()) {
             case LEFT, RIGHT, UP, DOWN -> e.consume(); //stops it from triggering further actions
-            default -> { return;
+            default -> {
+                return;
             }
         }
 
         boolean moved = switch (e.getCode()) { // yoinks the logic about movement from engine and initializes it
-            case LEFT  -> engine.moveLeft();
+            case LEFT -> engine.moveLeft();
             case RIGHT -> engine.moveRight();
-            case UP    -> engine.moveUp();
-            case DOWN  -> engine.moveDown();
-            default    -> false;
+            case UP -> engine.moveUp();
+            case DOWN -> engine.moveDown();
+            default -> false;
         };
         if (moved) { // SPAWNS THE TILES YAYAYAAYYY
             e.consume();
             engine.spawnTile();
             render(); // updates display
-
-            for (int[] cell : engine.getLastMerges()){
-                TileMergeAnm(tiles[cell[0]][cell[1]]);
-            }
-
             int r = engine.getLastRow(); //yoinks the last row and column tracker from engine
             int c = engine.getLastCol();
-
-            if( r >= 0 && c >= 0 ) { // safety check to ensure that the rows and columns indices(index) are non-negative(aka clear)
-                SpawnTileAnm(tiles[r][c]);
+            if (r >= 0 && c >= 0) { // safety check to ensure that the rows and columns indices(index) are non-negative(aka clear)
+                ANIMATION(tiles[r][c]);
             }
         }
     }
@@ -92,58 +91,73 @@ public class GameController {
                     Label lbl = new Label(String.valueOf(value));
                     lbl.setStyle("-fx-font-size: 24px; -fx-font-weight: bold; -fx-background-radius: 6; ");
                     tile.getChildren().add(lbl);
-                    tile.setStyle("-fx-background-radius: 6; -fx-background-color: " + tileColor(value) + ";" );
+                    tile.setStyle("-fx-background-radius: 6; -fx-background-color: " + tileColor(value) + ";");
                 }
             }
         }
     }
+
     private String tileColor(int val) { // colors and stuff for each tile
         return switch (val) {
-            case 3    -> "#dae8eeff";
-            case 6    -> "#b6e3f5ff";
-            case 12   -> "#70b2f1ff";
-            case 24   -> "#5b94ecff";
-            case 48   -> "#4276c7ff";
-            case 96   -> "#275cacff";
-            case 192  -> "#004cc5ff";
-            case 384  -> "#0062ffff";
-            case 768  -> "#0e78b8ff";
+            case 3 -> "#dae8eeff";
+            case 6 -> "#b6e3f5ff";
+            case 12 -> "#70b2f1ff";
+            case 24 -> "#5b94ecff";
+            case 48 -> "#4276c7ff";
+            case 96 -> "#275cacff";
+            case 192 -> "#004cc5ff";
+            case 384 -> "#0062ffff";
+            case 768 -> "#0e78b8ff";
             case 1536 -> "#1889ceff";
-            default   -> "#009fffff";
+            default -> "#009fffff";
         };
     }
 
     @FXML
     private void playAgain() {
+        engine = new GameEngine(); // starts the engine (spawns 2 tiles)
+
+        tiles = new StackPane[][]{ // maps the 16 child StackPane(aka tiles) into a 2D array
+                {tile1, tile2, tile3, tile4},
+                {tile5, tile6, tile7, tile8},
+                {tile9, tile10, tile11, tile12},
+                {tile13, tile14, tile15, tile16}
+        };
+
+        /*
+        This ensures that the key listener is added only after tile1 spawns which handles keyboard events in JavaFX,
+        stops the event from reaching any further nodes or handlers
+         */
+        tile1.sceneProperty().addListener((obs, oldScene, newScene) -> {
+            if (newScene != null) {
+                newScene.addEventFilter(KeyEvent.KEY_PRESSED, this::handleKeyPress); // intercepts events during the early "capture" phase of the event dispatch chain
+            }
+        });
+        render();
+        engine.spawnTile();
+    }
+
+    @FXML
+    private Label scoreLabel;
+
+    private void scoreCounter() {
+        int score = 0;
 
     }
 
-    private void SpawnTileAnm(StackPane tile) {
+    private void ANIMATION(StackPane tile) {
         // sets the initial tile coord size to 0
         tile.setScaleX(0);
         tile.setScaleY(0);
         //changes the amount of time it takes for the transition to happen
-        ScaleTransition inwardPop = new ScaleTransition(Duration.millis(200), tile);
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(150), tile);
         // original tile coord size(empty)
-        inwardPop.setFromX(0);
-        inwardPop.setFromY(0);
+        scaleTransition.setFromX(0);
+        scaleTransition.setFromY(0);
         // final tile coord size
-        inwardPop.setToX(1.0);
-        inwardPop.setToY(1.0);
-        inwardPop.play();
-
-    }
-
-    private void TileMergeAnm(StackPane tile) {
-        ScaleTransition pop = new ScaleTransition(Duration.millis(200), tile);
-        pop.setFromX(1.0);
-        pop.setFromY(1.0);
-        pop.setToX(1.2); // makes newly merged tile larger
-        pop.setToY(1.2);
-        pop.setAutoReverse(true); // Returns newly merged tile to original size
-        pop.setCycleCount(2);
-        pop.play();
-
+        scaleTransition.setToX(1.0);
+        scaleTransition.setToY(1.0);
+        scaleTransition.play();
     }
 
 }
